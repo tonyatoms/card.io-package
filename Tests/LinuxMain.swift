9ad6d7b06f804b547b3f39acd35dafeed316b11a
Tony Adams <tadams@mediafly.com>
2021-02-04 08:39:49 -0600

    remove more (mostly) swift files

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import card_io_packageTests
-
-var tests = [XCTestCaseEntry]()
-tests += card_io_packageTests.allTests()
-XCTMain(tests)
--- Tests/card.io-packageTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(card_io_packageTests.allTests),
-    ]
-}
-#endif
