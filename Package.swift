52bcd55b2f2872516638b8b9ebf09f9c06a18f9a
Tony Adams <tadams@mediafly.com>
2021-02-04 09:06:53 -0600

    reorg into targets by lang

--- Package.swift
+++ Package.swift
@@ -19,7 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "card.io-package",
+            name: "c",
             dependencies: ["cpplib"]),
+		.target(
+			name: "objc",
+			dependencies: ["cpplib"]),
     ]
 )
