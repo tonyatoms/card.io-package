0bc554d0e73ce4b9801793c6492d6ed24984b913
Tony Adams <tadams@mediafly.com>
2021-02-04 09:19:27 -0600

    simpler config

--- Package.swift
+++ Package.swift
@@ -4,25 +4,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "card.io-package",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "card.io-package",
-            targets: ["c", "objc"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "c",
-            dependencies: ["cpplib"]),
-		.target(
-			name: "objc",
-			dependencies: ["cpplib"]),
-    ]
+	name: "card.io-package",
+	targets: [.target(name: "c", dependencies:["cpplib"]),
+			  .target(name: "objc", dependencies:["cpplib"]),
+	]
 )
