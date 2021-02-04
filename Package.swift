ff1ec842eb21487cf49ea477bebbac3f1e9ce0d3
Tony Adams <tadams@mediafly.com>
2021-02-04 09:11:57 -0600

    revise library config

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "card.io-package",
-            targets: ["card.io-package"]),
+            targets: ["c", "objc"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
